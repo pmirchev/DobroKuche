894f58cc2689f06725bb00d3a743afb88e7513bc
Pavel Mirchev <pavel_mirchev@abv.bg>
2022-11-05 10:49:01 +0200

    Add project files.

---
+++ DobroKuche.WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace DobroKuche.WebApp.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
