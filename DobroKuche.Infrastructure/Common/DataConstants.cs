46bf92a38fbabcef2df3de19d94b1c4096ec1b4d
Pavel Mirchev <pavel_mirchev@abv.bg>
2022-11-05 11:49:40 +0200

    Adding First and Last name of the users

---
+++ DobroKuche.Infrastructure/Common/DataConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DobroKuche.Infrastructure.Common
+{
+    public static class DataConstants
+    {
+        public const int FirstNameMinLenght = 2;
+        public const int FirstNameMaxLenght = 20;
+        public const int LastNameMinLenght = 2;
+        public const int LastNameMaxLenght = 20;
+    }
+}
---
+++ DobroKuche.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace DobroKuche.Infrastructure.Data
+{
+    using DobroKuche.Infrastructure.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext<WebAppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
